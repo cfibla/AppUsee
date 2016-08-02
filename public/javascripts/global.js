b1e9da8e49bebf5405e15468913a79f07a46e406
Carlos <charliesuper@hotmail.com>
2016-08-02 14:34:57 +0200

    put_7

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,5 +1,14 @@
  $(document).ready(function (){
 
+
+ 	function checkCond(id){
+                  var cc='';
+                  if (alumne.checks[id] === true) {
+                  cc ="checked";
+                  }
+                  return cc;
+                  }
+
 $(":checkbox").change(function () 
    {
     alert('I have been checked');
