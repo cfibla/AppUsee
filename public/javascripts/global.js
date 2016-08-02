f8216a9171651a8465b73fd7220a4678ebbcb67b
Carlos <charliesuper@hotmail.com>
2016-08-02 23:04:49 +0200

    put_25

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -3,14 +3,14 @@
 $(":checkbox").change(function () 
    {
    
-
+/*
     if('input[type=checkbox]').is(':checked')){
    $(this).prop('checked',true);
  }else{
    $(this).prop('checked',false);
 
 
-
+*/
    // alert('I have been checked');
     })
 
