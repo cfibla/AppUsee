a587b3e21e10c731bbfe6ba287bd5e166939d532
Carlos <charliesuper@hotmail.com>
2016-08-02 14:36:51 +0200

    put_8

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,17 +1,16 @@
  $(document).ready(function (){
 
-
- 	function checkCond(id){
-                  var cc='';
-                  if (alumne.checks[id] === true) {
-                  cc ="checked";
-                  }
-                  return cc;
-                  }
-
 $(":checkbox").change(function () 
    {
     alert('I have been checked');
     })
 
      });
+
+ function checkCond(id){
+                  var cc='';
+                  if (alumne.checks[id] === true) {
+                  cc ="checked";
+                  }
+                  return cc;
+                  }
