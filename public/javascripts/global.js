3b31d57db0d0e60dafa6b00f2783a9aa655fbb61
Carlos <charliesuper@hotmail.com>
2016-08-02 23:15:23 +0200

    put_27

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -3,15 +3,15 @@
 $(":checkbox").change(function () 
    {
    
-/*
-    if('input[type=checkbox]').is(':checked')){
+
+   if(('input[type=checkbox]').is(':checked')) {
    $(this).prop('checked',true);
- }else{
-   $(this).prop('checked',false);
+ 	} else {
+   $(this).prop('checked',false);}
 
 
-*/
+
    // alert('I have been checked');
-    })
+ });
 
      });
