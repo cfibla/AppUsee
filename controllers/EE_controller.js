306d980e1548d2a8fc0d4be1f5b61e421c955e48
Carlos <charliesuper@hotmail.com>
2016-08-22 19:42:59 +0200

    seguiment_32

--- controllers/EE_controller.js
+++ controllers/EE_controller.js
@@ -99,7 +99,7 @@ exports.update = function (req, res){
 
 	models.Alumne.findByIdAndUpdate(alumneId,
 									{
-										$set: {checks:[alum.checks]},
+										$set: {checks:{[alum.checks]}},
 										altresEsp: alum.altresEsp,
 										atServPrivats: alum.atServPrivats,
 										percentDim: alum.percentDim,
