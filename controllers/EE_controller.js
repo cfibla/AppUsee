a1b4dc3944c792423d27b958f7c64602b16e11ff
Carlos <charliesuper@hotmail.com>
2016-08-23 13:27:00 +0200

    seguiment_87

--- controllers/EE_controller.js
+++ controllers/EE_controller.js
@@ -91,9 +91,9 @@ exports.update = function (req, res){
 
 	delete alum.id;
 	delete alum._id;
-	delete alum.segActuacions[];
-	delete alum.segInformacioCAD[];
-	delete alum.segAltresCoord[];
+	delete alum.segActuacions;
+	delete alum.segInformacioCAD;
+	delete alum.segAltresCoord;
 
 	models.Alumne.findByIdAndUpdate(alumneId, alum, {multi:true, new: true, safe: true, upsert: true},
 
