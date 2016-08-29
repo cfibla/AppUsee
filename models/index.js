7bc853ac50cd82fc0868598414dd6d7f419c91c3
Carlos <charliesuper@hotmail.com>
2016-08-29 14:24:47 +0200

    final_04

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 var mongoose = require ('mongoose');
 var schema = require ('./schema');
 
-mongoose.connect('mongodb://localhost:27017/test');
-//mongoose.connect('mongodb://heroku_g5r9dhbr:8m2i3fvusespuik77q2q5dctj4@ds153785.mlab.com:53785/heroku_g5r9dhbr');
+//mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connect('mongodb://heroku_g5r9dhbr:8m2i3fvusespuik77q2q5dctj4@ds153785.mlab.com:53785/heroku_g5r9dhbr');
 
 
 //Els paràmetres són: nom model, esquema, collection
