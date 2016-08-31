72d22950933eedeba56588c3a9c1b1114a9ccae2
cfibla <charliesuper@hotmail.com>
2016-08-31 21:42:58 +0200

    imprimir prova 50

--- controllers/EE_controller.js
+++ controllers/EE_controller.js
@@ -135,7 +135,7 @@ exports.actuaPost = function (req, res) {
 
 }
 
-//IMPRIMIR
+//IMPRIMIR PDF
 exports.print = function (req, res) {
 	var alumneId = req.params.id;
 	models.Alumne.findById(alumneId, function(error, alumne){
@@ -143,10 +143,86 @@ exports.print = function (req, res) {
 			return res.json(error);
 		} else {
 
+				//require dependencies
+				var PDFDocument = require ('pdfkit');
+				var fs = require('fs');
+				var doc = new PDFDocument();
+				var tempFile = ('temp/PDFtemp.pdf');
 
-					res.render('pdf');
+				//FORMAT DATA NAIXEMENT
+				var dataNa = alumne.dataNaixement;
+                var dd = dataNa.getDate();
+                var mm = dataNa.getMonth()+1; //January is 0!
+                var yyyy = dataNa.getFullYear();
+                if(dd<10) {
+                    dd='0'+dd
+                    } 
+                if(mm<10) {
+                    mm='0'+mm
+                    } 
+                dataNa = dd+'/'+mm+'/'+yyyy;
+
+
+                //FORMAT CHECKS
+                var checksArr = [];
+                for (var i = 0; i < alumne.checks.length; i++) {
+                	if (alumne.checks[i] === true){
+                		checksArr[i] = 'X';
+                	} else {
+                		checksArr[i] = ' ';
+
+                	}
+                };
+				console.log(checksArr);
+
+					var nom = alumne.nomAlumne + ' ' + alumne.cognomAlumne1 + ' ' + alumne.cognomAlumne2;
+
+                    //creating a new PDF object
+					doc.pipe(fs.createWriteStream(tempFile));  //creating a write stream 
+					            //to write the content on the file system
+
+					//TITOL
+					doc.text('FITXA SEGUIMENT ALUMNES EE-USEE',{
+						align: 'center',
+						underline: 1
+					}); 
+					doc.text('Escola Montserrat Vayreda',{
+						align: 'center'
+					}); 
+					doc.moveDown(2.5);
+
+					//DADES ALUMNE
+					doc.text('Dades personals', {underline: 1});
+
+					doc.text('Nom: '+ nom);
+					doc.text('Data de naixement: '+ dataNa);
+					doc.text('Targeta de la Seguretat Social: '+ alumne.seguretatSoc);
+					doc.moveDown(1.5);
+
+					//ATENCIÓ EE-USEE
+					doc.text('Atenció EE-USEE', {underline: 1});
+					doc.text('Seguiment atenció a la diversitat');
+					doc.text('Directe: '+ checksArr[0]);
+					doc.text('Indirecte: '+ checksArr[1]);
+					doc.text('Atenció');
+					doc.text('AiLL: '+ checksArr[2]);
+					doc.text('EE: '+ checksArr[3]);
+					doc.text('USEE: '+ checksArr[4]);
+
+
+
+
+
+					doc.end(); //we end the document writing.
+
 		}
 	});
+					var fs = require('fs');
+					var tempFile = ('temp/PDFtemp.pdf'); 
+					fs.readFile(tempFile, function (err,data){
+								    res.contentType("application/pdf");
+								    res.send(data);
+								    });
 };
 
 //Suprimir alumne - VIEW
