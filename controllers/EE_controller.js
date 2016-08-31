f0ff7bc37b72e38c17051e4d35e3976f552b4510
cfibla <charliesuper@hotmail.com>
2016-08-31 12:16:46 +0200

    imprimir prova 4

--- controllers/EE_controller.js
+++ controllers/EE_controller.js
@@ -157,14 +157,14 @@ exports.print = function (req, res) {
 				var lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam in suscipit purus.  Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Vivamus nec hendrerit felis. Morbi aliquam facilisis risus eu lacinia. Sed eu leo in turpis fringilla hendrerit. Ut nec accumsan nisl.'
 
 				doc.fontSize = 8;
-				doc.text ('This text is left aligned. ' + lorem,
-								  width: 410,
-								  align: 'left');
+				doc.text ('This text is left aligned. ' + lorem,{
+												  width: 410,
+												  align: 'left'});
 
 				doc.moveDown();
-				doc.text ('This text is centered. ' + lorem,
-								  width: 410,
-								  align: 'center');
+				doc.text ('This text is centered. ' + lorem,{
+												  width: 410,
+												  align: 'center'});
 
 				//get a blob when you're done
 				doc.end()
