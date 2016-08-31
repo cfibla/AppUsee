df9b935101b0dc30b8392cafcd5938cd99900c2b
cfibla <charliesuper@hotmail.com>
2016-08-31 12:23:08 +0200

    imprimir prova 7

--- controllers/EE_controller.js
+++ controllers/EE_controller.js
@@ -149,8 +149,8 @@ exports.print = function (req, res) {
 					
 					var text = 'ANY_TEXT_YOU_WANT_TO_WRITE_IN_PDF_DOC';
 
-					doc = new PDF();                        //creating a new PDF object
-					doc.pipe(fs.createWriteStream('PATH_TO_PDF_FILE'));  //creating a write stream 
+					doc = new PDFDocument();                        //creating a new PDF object
+					doc.pipe(fs.createWriteStream('../PDF/Doc.pdf'));  //creating a write stream 
 					            //to write the content on the file system
 					doc.text(text, 100, 100);             //adding the text to be written, 
 					            // more things can be added here including new pages
