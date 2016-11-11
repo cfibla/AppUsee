bf86032edd9503f6c75b1ca2b6ac00c18c6fb0d0
cfibla <charliesuper@hotmail.com>
2016-11-11 22:36:59 +0100

    assistData 11_11_16

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,20 +1,35 @@
 $(document).ready(function (){
    // bind to retrieve old status
-$('input[type="radio"]').mousedown(function() { 
-    // if it was checked before
-    if(this.checked) {
-        // bind event to reset state after click is completed
-        $(this).mouseup(function() {  
-            // bind param, because "this" will point somewhere else in setTimeout
-            var radio = this;
-            // apparently if you do it immediatelly, it will be overriden, hence wait a tiny bit
-            setTimeout(function() { 
-                radio.checked = false; 
-            }, 5); 
-            // don't handle mouseup anymore unless bound again
-            $(this).unbind('mouseup');
-        });
-    }
+
+    $('input[type="radio"]').mousedown(function() { 
+        // if it was checked before
+        if(this.checked) {
+            // bind event to reset state after click is completed
+            $(this).mouseup(function() {  
+                // bind param, because "this" will point somewhere else in setTimeout
+                var radio = this;
+                // apparently if you do it immediatelly, it will be overriden, hence wait a tiny bit
+                setTimeout(function() { 
+                    radio.checked = false; 
+                }, 5); 
+                // don't handle mouseup anymore unless bound again
+                $(this).unbind('mouseup');
+            });
+        }
+    });
+
+
+
+    $('#data .input-group.date').datepicker({
+        format: "dd/mm/yy",
+        maxViewMode: 2,
+        todayBtn: "linked",
+        language: "ca",
+        daysOfWeekDisabled: "0,6",
+        todayHighlight: true
+    });
+
+
 });
 
 /*
@@ -34,7 +49,7 @@ $(":radio").click(function () {
 
  });
  */
-});
+
 
 /*$(":submit").click(function () {
    if($("#actuacionsBody").val('')){
