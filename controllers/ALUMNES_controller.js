c51db84111ebd324793e6699d723c3e0c41dc1f6
cfibla <charliesuper@hotmail.com>
2016-11-27 12:47:40 +0100

    assistData 27_11_16 TEST 6

--- controllers/ALUMNES_controller.js
+++ controllers/ALUMNES_controller.js
@@ -212,7 +212,7 @@ exports.assisPost = function (req, res) {
 				if (error){
 					res.send(error)
 				} else {
-					console.log (assDa);
+					console.log (JSON.stringify(assDa));
 				}
 			})
 
