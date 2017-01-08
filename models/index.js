047c184ec6889cf08022230acb0df4ebd3132eb4
cfibla <charliesuper@hotmail.com>
2017-01-09 00:19:48 +0100

    8_1_17 ok2

--- models/index.js
+++ models/index.js
@@ -6,8 +6,8 @@ var schemaescoles = require('./schema_escoles');
 var schemaesp = require('./schema_usersEsp');
 var schemaee = require('./schema_usersEe');
 
-mongoose.connect('mongodb://localhost:27017/test');
-//mongoose.connect('mongodb://heroku_g5r9dhbr:8m2i3fvusespuik77q2q5dctj4@ds153785.mlab.com:53785/heroku_g5r9dhbr');
+//mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connect('mongodb://heroku_g5r9dhbr:8m2i3fvusespuik77q2q5dctj4@ds153785.mlab.com:53785/heroku_g5r9dhbr');
 
 
 //Els paràmetres són: nom model, esquema, collection
