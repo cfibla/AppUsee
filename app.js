4f317e4a191dc0510db54e56e7201b71a33813b9
cfibla <charliesuper@hotmail.com>
2017-08-01 12:02:15 +0200

    1_8_17 v2

--- app.js
+++ app.js
@@ -72,12 +72,5 @@ app.use(function(err, req, res, next) {
     });
 });
 
-app.use(function(req, res, next) {
-  if(!req.secure) {
-    return res.redirect(['https://', req.get('Host'), req.url].join(''));
-  }
-  next();
-});
-
 
 module.exports = app;
