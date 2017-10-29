6ca814e4d2086335eb0b59fdf874ed9273f29a47
cfibla <charliesuper@hotmail.com>
2017-10-29 22:20:16 +0100

    29102017 protect 2

--- models/index.js
+++ models/index.js
@@ -9,8 +9,8 @@ var schemaEe = require('./schema_usersEe');
 
 var schemaHorari = require('./schema_horari');
 
-mongoose.connect('mongodb://localhost:27017/test');
-//mongoose.connect('mongodb://heroku_g5r9dhbr:8m2i3fvusespuik77q2q5dctj4@ds153785.mlab.com:53785/heroku_g5r9dhbr');
+//mongoose.connect('mongodb://localhost:27017/test');
+mongoose.connect('mongodb://heroku_g5r9dhbr:8m2i3fvusespuik77q2q5dctj4@ds153785.mlab.com:53785/heroku_g5r9dhbr');
 
 //Els paràmetres són: nom model, esquema, collection
 exports.Alumne = mongoose.model('Alumne', schema, 'Alumnes');
