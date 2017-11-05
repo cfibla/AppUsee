0652688f90b70a087a16b1e48b1d956b2a6e61eb
cfibla <charliesuper@hotmail.com>
2017-11-05 13:29:01 +0100

    05112017

--- Readme.txt
+++ Readme.txt
@@ -1,23 +1,14 @@
-branch --> windows
+branch --> horaris
 
 GENERAL
-Faldón
-ARREGLAR ESCOLES!!! schema alumnes->type: mongoose.Schema.ObjectId,
-/*Merge users Tutor-USEE
-Agenda
-/*botons inici (home)
+
+menú lateral if session
+
 Perfil usuari -> Escola (crear nova si canvia) - Companys
 Uppercase modificació dades
 Comunicar errors SIGN UP
 
 
-AULA ORDINARIA
-Modals Assistència i Menjador
-Imprimir assistència diària
-/*Imprimir menjador diari
-/*Imprimir reunions pares
-
-USEE
 
 USUARIS DEMO
 demo@demo.cat
--- models/schema_horari.js
+++ models/schema_horari.js
@@ -6,32 +6,50 @@ module.exports = new mongoose.Schema({
 			data: String,
 			dia: Number,
 			h1: String,
-			clase1: String,
+			clase1:  {
+				type: String,
+				uppercase: true
+			},
 			prog1: String,
 			ref1: String,
 			session1: String,
 			h2: String,
-			clase2: String,
+			clase2:   {
+				type: String,
+				uppercase: true
+			},
 			prog2: String,
 			ref2: String,
 			session2: String,
 			h3: String,
-			clase3: String,
+			clase3:   {
+				type: String,
+				uppercase: true
+			},
 			prog3: String,
 			ref3: String,
 			session3: String,
 			h4: String,
-			clase4: String,
+			clase4:   {
+				type: String,
+				uppercase: true
+			},
 			prog4: String,
 			ref4: String,
 			session4: String,
 			h5: String,
-			clase5: String,
+			clase5:   {
+				type: String,
+				uppercase: true
+			},
 			prog5: String,
 			ref5: String,
 			session5: String,
 			h6: String,
-			clase6: String,
+			clase6:   {
+				type: String,
+				uppercase: true
+			},
 			prog6: String,
 			ref6: String,
 			session6: String,
