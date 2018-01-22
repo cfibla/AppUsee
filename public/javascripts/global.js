af968dda26f61ef92237aa0a89cfb68137b71ce8
Txals <charliesuper@hotmail.com>
2018-01-22 14:07:25 +0100

    22012018cole

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -173,6 +173,22 @@ $(document).ready(function (){
       }
   });
 
+////////////////////LOGIN///////////////////////////
+    $('#login-form-link').click(function(e) {
+    $("#login-form").delay(100).fadeIn(100);
+    $("#register-form").fadeOut(100);
+    $('#register-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+  $('#register-form-link').click(function(e) {
+    $("#register-form").delay(100).fadeIn(100);
+    $("#login-form").fadeOut(100);
+    $('#login-form-link').removeClass('active');
+    $(this).addClass('active');
+    e.preventDefault();
+  });
+
 ///////////////////// M O D A L S ///////////////////// 
   $('body').on('click.modal.data-api', '[data-toggle="modal"]', function(){
      $($(this).data("target")+' .modal-content').load($(this).attr('href'));
